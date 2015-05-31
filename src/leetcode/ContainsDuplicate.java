aac9b74d2dd840e5dd96e737b738407b0daaba2f
wangt <wangtao.upc@gmail.com>
2015-05-31 11:55:37 +0800

    Fix compile error

--- src/leetcode/ContainsDuplicate.java
+++ src/leetcode/ContainsDuplicate.java
@@ -1,23 +1,28 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
-* https://leetcode.com/problems/contains-duplicate/
-*
-**/
+ * https://leetcode.com/problems/contains-duplicate/
+ *
+ **/
 public class ContainsDuplicate {
-    public boolean containsDuplicate(int[] nums) {
-        if(nums == null || nums.length == 0){
-            return false;
-        }
-        
-        Set<Integer> numSet = new HashSet<Integer>();
-        
-        for(int i : nums){
-            if(numSet.contains(i)){
-                return true;
-            }else{
-                numSet.add(i);
-            }
-        }
-        
-        return false;
-    }
+	public boolean containsDuplicate(int[] nums) {
+		if (nums == null || nums.length == 0) {
+			return false;
+		}
+
+		Set<Integer> numSet = new HashSet<Integer>();
+
+		for (int i : nums) {
+			if (numSet.contains(i)) {
+				return true;
+			} else {
+				numSet.add(i);
+			}
+		}
+
+		return false;
+	}
 }
